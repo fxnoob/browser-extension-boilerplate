527c0011e2ef800db796a813d9b4a541750ae131
fxnoob <fxnoob71@gmail.com>
2019-08-30 21:04:30 +0530

    update

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,19 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ['babel-loader']
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
+          },
+        ]
       }
     ]
   },
