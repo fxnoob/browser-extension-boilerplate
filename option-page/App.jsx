fde125d21738ddac2c3235f5532e86afa20d222d
fxnoob <fxnoob71@gmail.com>
2019-11-03 23:28:39 +0530

    prettier fix

--- option-page/App.jsx
+++ option-page/App.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Index from './components/'
+import React from "react";
+import ReactDOM from "react-dom";
+import Index from "./components/";
 
 const Element = document.createElement("div");
-Element.setAttribute("id" , "dfghbnjmERHJKFGHNMVBNMFBNMbmvvxnbdgf");
+Element.setAttribute("id", "dfghbnjmERHJKFGHNMVBNMFBNMbmvvxnbdgf");
 document.body.appendChild(Element);
-ReactDOM.render(<Index />, document.getElementById("dfghbnjmERHJKFGHNMVBNMFBNMbmvvxnbdgf"));
+ReactDOM.render(
+  <Index />,
+  document.getElementById("dfghbnjmERHJKFGHNMVBNMFBNMbmvvxnbdgf")
+);
